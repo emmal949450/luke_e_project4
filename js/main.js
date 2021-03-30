249e08922613369d1f1ef7b357c00f18dbf2312e
Emma Luke <eluke949@gmail.com>
2021-03-30 11:51:06 -0400

    adding

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+
+(function(){
+	"use strict";
+	console.log("fired");
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burgerCon")
+
+	function menu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", menu);
+
+})();
